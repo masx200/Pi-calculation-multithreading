d8f5842dd95e09802f539c08c98495e65ff81e8c
masx200 <34191203+masx200@users.noreply.github.com>
2022-03-12 15:15:58 +0800

    升级

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,11 @@ import { resolve } from "path";
 import { defineConfig } from "vite";
 import { minifyHtml } from "vite-plugin-html";
 export default defineConfig({
+    esbuild: { drop: ["console", "debugger"] },
     plugins: [minifyHtml({ removeAttributeQuotes: false })],
     root: resolve(__dirname, "src"),
     build: {
+        minify: "esbuild",
         target: "es2015",
         terserOptions: {
             compress: { drop_console: true, drop_debugger: true },
